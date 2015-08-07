0279db36174beae86d78d269be036fa2361a497a
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-08-08 07:10:34 +0900

    fix settings for rubocop.

--- init.el
+++ init.el
@@ -373,22 +373,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Ruby
 
-(flycheck-define-checker ruby-rubocop
-  "A Ruby syntax and style checker using the RuboCop tool."
-  :command ("rubocop" "--format" "emacs"
-            (config-file "--config" flycheck-rubocoprc)
-            source)
-  :error-patterns
-  ((warning line-start
-            (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
-            line-end)
-   (error line-start
-          (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
-          line-end)))
-
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (rubocop-mode t)
+             (flycheck-define-checker ruby-rubocop
+               "A Ruby syntax and style checker using the RuboCop tool."
+               :command ("rubocop" "--format" "emacs"
+                         (config-file "--config" flycheck-rubocoprc)
+                         source)
+               :error-patterns
+               ((warning line-start
+                         (file-name) ":" line ":" column ": " (or "C" "W") ": " (message)
+                         line-end)
+                (error line-start
+                       (file-name) ":" line ":" column ": " (or "E" "F") ": " (message)
+                       line-end)))
              (setq flycheck-checker 'ruby-rubocop)
              (ruby-electric-mode t)
              (ruby-block-mode t)
