a80c06039a83d12e8ca49b96e8821742fa570151
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2015-09-26 19:36:39 +0900

    Use #+:quicklisp

--- init.lisp
+++ init.lisp
@@ -1,5 +1,8 @@
+#+:quicklisp
 (ql:quickload '(:prove :qlot) :silent t)
-(setf prove:*enable-colors* t)
-(ros:ignore-shebang)
-(setf sb-impl::*default-external-format* :utf-8)
-(setf sb-alien::*default-c-string-external-format* :utf-8)
+#+:quicklisp
+(progn
+  (setf prove:*enable-colors* t)
+  (ros:ignore-shebang)
+  (setf sb-impl::*default-external-format* :utf-8)
+  (setf sb-alien::*default-c-string-external-format* :utf-8))
