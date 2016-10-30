3b3563d034db05c41d1532df9416414769efda68
Rudolph-Miller <me@rudolph-miller.com>
2016-10-30 10:57:10 +0900

    export new configs

--- karabiner.sh
+++ karabiner.sh
@@ -2,16 +2,18 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.jis_command2eisuukana_prefer_command 1
-/bin/echo -n .
-$cli set repeat.wait 20
-/bin/echo -n .
-$cli set repeat.initial_wait 100
-/bin/echo -n .
-$cli set remap.swap_number_and_symbol 1
+$cli set private.app_sequelpro_control_h_to_backspace 1
 /bin/echo -n .
 $cli set remap.delete2forwarddelete 1
 /bin/echo -n .
+$cli set remap.jis_command2eisuukana_prefer_command 1
+/bin/echo -n .
 $cli set remap.qwerty2dvorak 1
+/bin/echo -n .
+$cli set remap.swap_number_and_symbol 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
+$cli set repeat.wait 33
 /bin/echo -n .
 /bin/echo
